057ebd6975125a9fc7d096b0eab6131daf1fdf94
Dascalov05 <96237702+Dascalov05@users.noreply.github.com>
2023-11-23 18:39:51 +0100

    Create php_es2_pag203.php

---
+++ Laboratorio 2023-2024/php_es-libro/php_es2_pag203.php
@@ -0,0 +1,36 @@
+<?php
+class Libro {
+    public $nomeLibro;
+    public $prezzo;
+    public $numeroScaffale;
+    public $numeroPagine;
+    public $casaEditrice;
+
+    public function inizializza($nomeLibro, $prezzo, $numeroScaffale, $numeroPagine, $casaEditrice) {
+        $this->nomeLibro = $nomeLibro;
+        $this->prezzo = $prezzo;
+        $this->numeroScaffale = $numeroScaffale;
+        $this->numeroPagine = $numeroPagine;
+        $this->casaEditrice = $casaEditrice;
+    }
+
+    public function mostra() {
+        echo "Nome: " . $this->nomeLibro . "\n";
+        echo "Prezzo: " . $this->prezzo . "\n";
+        echo "Scaffale: " . $this->numeroScaffale . "\n";
+        echo "Numero di pagine: " . $this->numeroPagine . "\n";
+        echo "Casa editrice: " . $this->casaEditrice . "\n";
+    }
+
+    public function applicaSconto() {
+        $this->prezzo = $this->prezzo - ($this->prezzo * 0.10);
+    }
+}
+
+$libro1 = new Libro();
+$libro1->inizializza("Diario di Anne Frank", 11.05, 3, 388, "Feltrinelli");
+$libro1->mostra();
+echo "-----\n";
+$libro1->applicaSconto();
+$libro1->mostra();
+?>
